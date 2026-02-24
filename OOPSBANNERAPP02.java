500926b5e508352dc7e8ea0e30bd961fa212b198
Ansh8081 <anshpawar81@gmail.com>
2026-02-24 13:17:26 +0530

    Initial commit

---
+++ OOPSBANNERAPP02.java
@@ -0,0 +1,11 @@
+public static void main(String[] args) {
+		System.out.println("OOPS");
+		// O           O           P            S
+        System.out.println(" ***  " + " " + " ***  " + " " + "*****  " + " " + " ***** ");
+        System.out.println("**  **" + " " + "**  **" + " " + "**  ** " + " " + "**     ");
+        System.out.println("**  **" + " " + "**  **" + " " + "**  ** " + " " + "**     ");
+        System.out.println("**  **" + " " + "**  **" + " " + "*****  " + " " + " ***** ");
+        System.out.println("**  **" + " " + "**  **" + " " + "**     " + " " + "    ** ");
+        System.out.println("**  **" + " " + "**  **" + " " + "**     " + " " + "    ** ");
+        System.out.println(" ***  " + " " + " ***  " + " " + "**     " + " " + " ***** ");
+	}
